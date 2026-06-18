c657f4c69189f784dabeefd7ea064ffa0895bd66
Dishmitha <dishmithamiryala@gmail.com>
2026-06-18 13:44:04 +0530

    Delete check_dataset.py

--- check_dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("sign_data.csv", header=None)
-
-print("Rows:", len(df))
-print("Columns:", len(df.columns))
-
-print("\nSamples per letter:")
-print(df.iloc[:, -1].value_counts().sort_index())
